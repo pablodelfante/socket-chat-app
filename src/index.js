41f24c70ebb9b9cd8ed124600b758a0cf1d772c9
pablo <pablodelfantexp@gmail.com>
2021-01-31 12:16:11 -0300

    agregue sistema de login para asignar alias de chat

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const app = express();
 const http = require('http').createServer(app);
 // le paso el server http a io
 const io = require('socket.io')(http);
+
+const cookieParser = require('cookie-parser')
+
 // defino puerto
 const port = process.env.PORT || 8080;
 
@@ -15,11 +18,25 @@ const port = process.env.PORT || 8080;
 // static es mi via de montaje para archivos estaticos
 app.use('/static', express.static(__dirname + '/public'));
 
+// sesion
+app.set('title', 'My Site')
+app.get('title') // "My Site"
+app.use(cookieParser())
 
-// PRIMER RUTA
+
+// RUTAS
+// login
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
+// chat
+app.get('/chat:param', (req, res) => {
+    console.log(req.params.param);
+    res.sendFile(__dirname + '/index.html');
+
+    // seteo la cookie desde el backend
+    res.cookie('prueba',req.params.param);
+});//fin de /chat
 
 
 // MENSAJES
@@ -29,13 +46,14 @@ io.on('connection', (socket) => {
     // una vez conectado el socket...
     console.log('alguien se ha conectado');
 
+
     socket.on('newMessage', msg => {
-        messages.push(msg);
-        
+        console.log(msg)
         // envio ultimo mensaje a todos!
         io.emit('message', msg);
     });
 });
+
 
 // ESCUCHA DE PUERTO
 http.listen(port, () => {
